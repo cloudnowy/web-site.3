50123ebd45d91620e625030b80a4b57edf9c5775
cloudnowy <vladimirponomarev185@gmail.com>
2025-01-10 00:42:21 +0300

    Создал страницу для визуализации обработки, удалил лишнии файлы, создал макет профиля,

--- test-deleting.js
+++
@@ -1,8 +0,0 @@
-// Функция для удаления теста
-function deleteTest(index) {
-    if (confirm('Вы уверены, что хотите удалить этот тест?')) {
-        tests.splice(index, 1);
-        localStorage.setItem('tests', JSON.stringify(tests));
-        renderTests();
-    }
-}
